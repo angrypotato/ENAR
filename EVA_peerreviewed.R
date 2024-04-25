cbb7cf9e890bdf5ea8b8c48f54344e64b596744c
Zoe Haskell-Craig <zoe.haskellcraig@gmail.com>
2024-04-25 10:19:48 -0400

    created new script for EVA for peer reviewed publication

---
+++ EVA_peerreviewed.R
@@ -0,0 +1,12 @@
+##################################
+## EVA for peer-reviewed paper
+## Authors: Zoe Haskell-Craig
+## Date Created: Dec 13th 2023
+## Last Modified: Feb 26th 2024
+##################################
+
+# packages --------
+library(readr) #read csv files
+library(tidyverse) #data manipulation 
+library(POT) #peaks over thresholds methods
+library(ggpubr) #arranging plots
